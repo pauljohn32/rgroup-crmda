1d1aebe136d71a928fecc483d2e2ab88410591ec
Paul Johnson <pauljohn@ku.edu>
2011-10-22 18:36:47 +0000

    persp-multicollinearity is new

---
+++ R-documentation/WorkingExamples/persp-multicollinearity-1.R
@@ -0,0 +1,208 @@
+## multicollinearity-plots.R
+## 3-dimensional demonstration of multicollinearity using persp
+## Author: Paul Johnson
+## Date: 2011-10-22
+
+## This is a set of functions that faciliates the examination
+## of multicollinearity. The "true" relationship is
+##
+##  y[i] = 0.2 * x1[i] + 0.2 * x2[i] + e
+##
+## where e is Normal(0, stde^2).  The slope coefficients
+## are written into the function genCorrelatedData, the
+## error standard deviation stde can be set as a user
+## argument for that function.
+
+## If you run through this whole thing, you will be
+## asked by R to hit return to see the next graph
+## several times.
+
+## The main objective here is to demonstrate multicollinearity to
+## regression students.
+
+
+magRange <- function(x, mult = 1.25){
+  xr <- range(x)
+  magXr <- xr +  c(-1, 1) * (mult-1) * diff(xr)
+  magXr
+}
+
+plotSeq <- function(x, length.out=length(x)){
+  xr <- range(x)
+  pseq <- seq(xr[1], xr[2], length.out=length.out)
+  pseq
+}
+
+
+
+mcGraph1 <- function (x1, x2, y, theta=0, phi=15){
+  x1range <- magRange(x1, 1.25)
+  x2range <- magRange(x2, 1.25)
+  yrange <- magRange(y, 1.5)
+  
+  zZero <- outer( plotSeq(x1range, l=5), plotSeq(x2range, l=5), function( a,b) { a*b*0 + yrange[1] } )
+  
+  res <- persp(x=plotSeq(x1range, l=5), y= plotSeq(x2range, l=5), z=zZero, zlim=yrange, lwd=1, xlab="x1",ylab="x2",zlab="y", theta=theta, phi=phi)
+  
+  yMinimum <- rep(yrange[1] , length(dx1))
+  mypoints1 <- trans3d ( x1, x2, yMinimum, pmat = res )
+  points( mypoints1, pch = 16, col= "blue")
+}
+
+
+
+mcGraph2 <- function(x1,x2,y, shrinky=1, theta=0, phi=15){
+  x1range <- magRange(x1, 1.25)
+  x2range <- magRange(x2, 1.25)
+  yrange <- magRange(y, 1.5)
+  
+##
+  zZero <- outer( plotSeq(x1range, l = 5), plotSeq(x2range, l = 5), function( a,b) { a*b*0 + yrange[1] } )
+
+  res <- persp(x = plotSeq(x1range, l = 5), y = plotSeq(x2range, l = 5), z = zZero, zlim = yrange, lwd = 1, xlab = "x1", ylab = "x2", zlab = "y", theta = theta, phi=phi)
+  
+  mypoints1 <- trans3d ( x1, x2 ,yrange[1], pmat = res )
+  newy <- shrinky * (y - yrange[1]) + yrange[1]
+  mypoints2 <- trans3d ( x1 , x2 , newy , pmat = res )
+  points( mypoints2, pch = 1, col= "blue")
+  points( mypoints1, pch = 16, col=gray(0.8))
+  
+  mypoints2s <- trans3d ( x1, x2, (0.8)*newy, pmat =res )
+  arrows ( mypoints1$x , mypoints1$y , mypoints2s$x , mypoints2s$y , col="red" , lty = 2, lwd=0.3, length=0.1)
+}
+
+
+
+
+mcGraph3 <- function(x1, x2, y, theta = 0, phi = 15){
+  x1range <- magRange(x1, 1.25)
+  x2range <- magRange(x2, 1.25)
+  yrange <- magRange(y, 1.5)
+  
+  
+  zZero <- outer( plotSeq(x1range, l = 5), plotSeq(x2range, l = 5), function( a, b) { a*b*0 + yrange[1] } )
+  
+  
+  res <- persp(x = plotSeq(x1range, l = 5), y = plotSeq(x2range, l = 5), z = zZero, zlim = yrange, lwd = 1, xlab = "x1", ylab = "x2", zlab = "y", theta = theta, phi = phi)
+  
+  mypoints1 <- trans3d( x1, x2, yrange[1], pmat = res )
+  
+  mypoints2 <- trans3d( x1, x2, y, pmat = res )
+  points( mypoints2, pch = 1, col = "blue")
+  points( mypoints1, pch = 16, col = gray(0.8))
+ 
+  m1 <- lm( y ~ x1 + x2)
+  # summary (m1)
+ 
+  x1seq <- plotSeq (x1range, length = 20)
+  x2seq <- plotSeq (x2range , length = 20)
+  
+  zplane <- outer ( x1seq, x2seq, function(a, b) { predict(m1,
+    newdata = data.frame( x1 = a, x2 = b ))} )
+  
+  for( i in 1:length(x1seq) ){
+    lines(trans3d(x1seq[i], x2seq, zplane[i,], pmat = res), lwd = 0.3)
+  }
+  for( j in 1:length(x2seq) ){
+    lines(trans3d(x1seq, x2seq[j], zplane[,j], pmat = res), lwd = 0.3)
+  }
+  
+  mypoints4 <- trans3d (x1 , x2 , fitted(m1) , pmat =res )
+##  points(mypoints4)
+  
+  mypoints2s <- trans3d ( x1, x2, y, pmat =res )
+  
+  arrows ( mypoints4$x , mypoints4$y , mypoints2s$x , mypoints2s$y , col = "red" , lty = 2, lwd = 0.3, length = 0.1)
+  summary(m1)
+}
+
+
+
+genCorrelatedData <- function(N = 100, means = c(50,50), sds = c(10,10), rho = 0.0, stde = 1){ 
+  require(MASS)
+  corr.mat <- matrix(c(1,rho,rho,1), nrow = 2)
+  stds.vec <- c(10,10)
+  sigma <- diag(sds) %*% corr.mat %*% diag(sds)
+  x.mat <-  mvrnorm(n = N, mu = means, Sigma = sigma)
+  y = 0 + 0.2* x.mat[,1] + 0.2 * x.mat[,2] + stde*rnorm (100,m = 0, s = 1)
+  dat <- data.frame(x.mat, y)
+  names(dat) <- c("x1", "x2", "y")
+  dat
+}
+  
+
+##
+##pdf(file="mcplots%03d.pdf", onefile=F, paper="special", height=6, width=6)
+
+## Now we get to use those functions to create
+## data and plots.
+
+
+## Create data with x1 and x2 correlated at 0.10
+dat <- genCorrelatedData(rho=.1, stde=10)
+
+mcGraph1(dat$x1, dat$x2, dat$y, theta=20, phi=8)
+
+
+## This will "grow" the "cloud" of points up from the
+## x1-x2 axis
+mcGraph2(dat$x1, dat$x2, dat$y, shrinky = 0.0, theta = 0) 
+mcGraph2(dat$x1, dat$x2, dat$y, shrinky = 0.1, theta = 0) 
+mcGraph2(dat$x1, dat$x2, dat$y, shrinky = 0.2, theta = 0)
+mcGraph2(dat$x1, dat$x2, dat$y, shrinky = 0.3, theta = 0) 
+mcGraph2(dat$x1, dat$x2, dat$y, shrinky = 0.4, theta = 0) 
+mcGraph2(dat$x1, dat$x2, dat$y, shrinky = 0.5, theta = 0)
+mcGraph2(dat$x1, dat$x2, dat$y, shrinky = 0.6, theta = 0) 
+mcGraph2(dat$x1, dat$x2, dat$y, shrinky = 0.7, theta = 0) 
+mcGraph2(dat$x1, dat$x2, dat$y, shrinky = 0.8, theta = 0)
+mcGraph2(dat$x1, dat$x2, dat$y, shrinky = 0.9, theta = 0) 
+mcGraph2(dat$x1, dat$x2, dat$y, shrinky = 1, theta = 0)
+ 
+
+mcGraph3(dat$x1, dat$x2, dat$y, theta = 0)
+
+dat2 <- genCorrelatedData(rho = 0, stde = 10)
+
+mcGraph3(dat2$x1, dat2$x2, dat2$y, theta = 0, phi = 10)
+
+mcGraph3(dat2$x1, dat2$x2, dat2$y, theta = 30, phi = 10)
+
+mcGraph3(dat2$x1, dat2$x2, dat2$y, theta = -30, phi = 10)
+
+mcGraph3(dat2$x1, dat2$x2, dat2$y, theta = -30, phi = -10)
+
+mcGraph3(dat2$x1, dat2$x2, dat2$y, theta = -30, phi = -15)
+
+
+
+## Now, back to work. Run 10 regressions with not-strongly
+## correlated data
+par(ask = T)
+
+modset1 <- list()
+for(i in 1:10){
+  dat2 <- genCorrelatedData(rho = .1, stde = 10)
+  summary(lm( y ~ x1 + x2 , data = dat2))
+  modset1[[i]] <- mcGraph3(dat2$x1, dat2$x2, dat2$y, theta = 0)
+}
+
+
+## Fit 10 regressions with strongly correlated data
+par(ask = T)
+
+modset2 <- list()
+for(i in 1:10){
+  dat2 <- genCorrelatedData(rho = .981, stde = 10)
+  summary(lm( y ~ x1 + x2 , data = dat2))
+  modset2[[i]] <- mcGraph3(dat2$x1, dat2$x2, dat2$y, theta = 0)
+}
+
+##Compare the estimated coefficients for model sets 1 and 2
+ lapply(modset1, coef)
+
+ lapply(modset2, coef)
+
+## Turn off the ask permission on plots
+par(ask = F)
+
+##dev.off()
--- R-documentation/WorkingExamples/psych-bifactor-01.R
+++ R-documentation/WorkingExamples/psych-bifactor-01.R
@@ -1,27 +1,22 @@
 ## Paul Johnson <pauljohn@ku.edu>
-## 2011-10-12
+## 2011-10-14
 
 ## Aim is to learn to generate "bi-factor" structured indicators
 ## for a cfa analysis and then simulate repeatedly.
 
 ## As it stands now, I believe sim.hierarchical is generating
-## the data correctly, but lavaan has trouble analyzing it.
-## I'm going to contact the authorities on the singularity in
-## lavaan's standard error calculations.  I'm also going
-## to see if an MPlus user will fit that model for me.
+## the data correctly. Still wrestling to get the lavaan
+## syntax to work properly (and match an equivalent MPlus specification).
 
 set.seed(12345)
 
 ## First, set the sample size.
 Nsample <- 500
 
-## Using the psych package by William Revelle. This is a widely known
-## collection of routines intended for psychometrics and psychology
-## more generally. I still don't understand the terminology
-## "group" and "hierarchical" very well. Still.
+## Using the psych package by William Revelle.
 
-## In this code, we explore
-## sim.hierarchical to generate data for "bi.factor" analysis
+## In this code, we explore sim.hierarchical to generate data for
+## "bi.factor" analysis
 
 library(psych)
 
@@ -42,29 +37,28 @@ library(psych)
 ## sets of rows. Assume f1, f2, and f3
 ## are orthogonal. 
 
-## how should I set "myg" and "myf" so that they get what they
-## want from sim.hierarchical?
 
-myf <- matrix( c( 0.4,0.4,0.5,0.3,0.5, 0.4, 0.2, 0.5,
-            0.2,0.4,0.4,0.4,rep(0,4),
-            rep(0,4), 0.3,0.5,0.3,0.5), ncol=3)
+myf <- matrix( c( 0.4,0.4,0.5,0.3,0.5,0.4,0.2,0.5,
+                 0.7,0.6,0.8,0.6,rep(0,4),
+                 rep(0,4), 0.7,0.7,0.5,0.7), ncol=3)
+
+
+###Caution: this gives data that leads to unstable cfa models
+#myf <- matrix( c( 0.4,0.4,0.5,0.3,0.5,0.4,0.2,0.5,
+#                 0.2,0.4,0.4,0.4,rep(0,4),
+#                 rep(0,4), 0.3,0.5,0.3,0.50), ncol=3)
+
 
 
 ### We want the three factors are all completely separate
 ### from sim.hierarical's group loading, "gload"
 myg <- c(0,0,0)
 
-
-
+## Grab the data.
 sim2 <- sim.hierarchical(gload=myg, fload=myf, n= Nsample, raw=T)
-sim2
-
-##Note that although they do not print to the screen from sim2,
-## the sample data is in there. It is sim2$observed
-str(sim2)
-
-## Grab the data so we can study it
 mydata <- as.data.frame(sim2$observed)
+
+# write.table(mydata, file="bifactor.txt", row.names=F)
 
 ## the omega graph shows that the "3 factor bi.factor" model
 ## can be re-done as a general factor with 2 special factors.
@@ -72,57 +66,120 @@ mydata <- as.data.frame(sim2$observed)
 omega(sim2$model)
 
 ##I can't understand this at all.
-##omega(sim2$model, sl=F)
-
+omega(sim2$model, sl=F)
 
 ## Analyze the data with CFA to see if we are getting what
 ## we ask for
 
 ## I think lavaan's method is more understandable than sem.
 
+sim2cov <- cov(sim2$observed)
+
+#analyze the "true" model
+## mycov <- sim2$model
+
+#Or pretend the observed correlation matrix is covar
+#myrcov <- sim2$r
+
+#######
+### retrieve saved data that can be fit successfully, if you need
+### a happy thought
+## mydata <- read.table("http://pj.freefaculty.org/R/sem-testing/psych-bifactor.txt",header=TRUE)
+
+
 library(lavaan)
 mymod  <- '
              f1 =~ V1 + V2 + V3 + V4 + V5 + V6 + V7 + V8
              f2 =~ V1 + V2 + V3 + V4 
+             f3 =~ V5 + V6 + V7 + V8
+'
+
+fit <- cfa(model.syntax=mymod, data=mydata, orthogonal=TRUE,
+           std.lv=TRUE)
+summary(fit, fit.measures=TRUE)
+
+
+
+
+
+
+
+
+mymod2  <- '
+             f1 =~ V1 + V2 + V3 + V4 + V5 + V6 + V7 + V8
+             f2 =~ V1 + V2 + V3 + V4
              f3 =~ V5 + V6 + V7 + V8
              f1 ~~ 0*f2
              f1 ~~ 0*f3
              f2 ~~ 0*f3
 '
 
-## lavaan
-##
+fit2 <- cfa(model.syntax=mymod,
+           data=mydata, orthogonal=TRUE,
+           std.lv=TRUE)
+summary(fit2, fit.measures=TRUE)
 
-sim2cov <- cov(sim2$observed)
 
-#analyze the "true" model
-mycov <- sim2$model
 
-#Or pretend the observed correlation matrix is covar
-#myrcov <- sim2$r
+##manually fixing the variances and freeing the loadings of the first
+## indicator
+mymod3  <- '
+             f1 =~ NA*V1 + V2 + V3 + V4 + V5 + V6 + V7 + V8
+             f2 =~ NA*V1 + V2 + V3 + V4
+             f3 =~ NA*V5 + V6 + V7 + V8
+             f1 ~~ 1*f1
+             f2 ~~ 1*f2
+             f3 ~~ 1*f3
+             f1 ~~ 0*f2
+             f1 ~~ 0*f3
+             f2 ~~ 0*f3
+'
 
-fit <- cfa(model.syntax=mymod,
-           sample.cov= sim2cov,
-           sample.nobs= Nsample,
+fit3 <- cfa(mymod3, data=mydata, mimic="Mplus")
+summary(fit3, fit.measures=TRUE)
+
+
+#change to observed information matrix
+fit4 <- cfa(model.syntax=mymod,
            mimic="Mplus",
-           missing="listwise",
            data=mydata, orthogonal=T,
-           std.lv=T)
+           std.lv=T, information="observed")
+
+summary(fit4, fit.measures=TRUE)
+#didn't converge
 
 
-summary(fit, fit.measures=TRUE)
-
-
-fit <- cfa(model.syntax=mymod,
-           sample.cov= sim2cov,
-           sample.nobs= Nsample,
+##try to tune up, control. fails. cfa quits iterating anyway
+fit5 <- cfa(model.syntax=mymod,
            mimic="Mplus",
-           missing="listwise",
-           sample.mean=mean(sim2$observed),
-           meanstructure=T,
-           control=list(optim.method="BFGS"),
            data=mydata, orthogonal=T,
-           std.lv=T)
+           std.lv=T, information="observed",
+            control=list(iter.max=100000, eval.max=100000))
+summary(fit5, fit.measures=TRUE)
 
 
-summary(fit, fit.measures=TRUE)
+fit6 <- cfa(model.syntax=mymod,
+           mimic="Mplus",
+           data=mydata, orthogonal=T,
+           std.lv=T, information="observed",
+            control=list(optim.method="BFGS"))
+
+
+summary(fit6, fit.measures=TRUE)
+
+
+
+
+
+getData <- function(myg=NULL, myf=NULL, Nsample=NULL){
+  sim2 <- sim.hierarchical(gload=myg, fload=myf, n= Nsample, raw=T)
+  mydata <- as.data.frame(sim2$observed)
+  fit <- cfa(model.syntax=mymod, data=mydata, orthogonal=TRUE,
+             std.lv=TRUE)
+  summary(fit, fit.measures=TRUE)
+  list(conv=fit@Fit@converged, fit)
+}
+
+
+res <- list()
+for(i in 1:10) res[[i]] <- getData(myg, myf, Nsample)
