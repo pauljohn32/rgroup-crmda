4cdcd30bf74e254b958884cd269b8db503757f5b
Paul Johnson <pauljohn@ku.edu>
2012-10-29 22:11:45 +0000

    major revisions, trying to recover what I lost (carelessly)

--- portableParallelSeeds/portableParallelSeeds/inst/examples/randomLesson-001.R
+++ portableParallelSeeds/portableParallelSeeds/inst/examples/randomLesson-001.R
@@ -1,12 +1,9 @@
 ## Paul Johnson
 ## 2012-10-24
 
-## R's default random generator is MT19937.  Because most people don't
-## seem to understand the difference between a seed value and the
-## internal state of the random generator, I've prepared this little
-## example program.  This illustrates the internal state of
-## MT19937 and discusses some ways to re-set that generator to
-## a given initial position.
+## R's default random generator is MT19937.  This illustrates the
+## internal state of MT19937 and discusses some ways to re-set that
+## generator to a given initial position.
 ##
 
 ## First, note that the R random generator is NOT actually created
@@ -24,18 +21,39 @@ set.seed(12345)
 ## position. But its internal state is NOT 12345. It is, instead
 ## a "state" vector that can be derived from 12345.
 
-## It is especially easy to generate random numbers from a uniform distribution. All we  have to do is take the randomly drawn positive integer (which the generator provides, because that's what generators do) and divide by the largest possible integer.
+## That number 12345, the thing we applied researchers call "the
+## seed", is actually no a seed. It is just some integer we
+## specify that R can use to construct the intial state of the
+## random generator.
+
+## Where to random draws come from? After you get the
+## equally-likely integers from the generator, that is, where
+## to random normals or poissons or gammas come from?
+
+## It is especially easy to generate random numbers from a uniform
+## distribution. All we have to do is take the randomly drawn positive
+## integer (which the generator provides, because that's what
+## generators do) and divide by the largest possible integer the
+## generator can provide. This is such a common service that
+## many generators will supply output within the [0,1] interval.
+## However, my background in the Swarm toolkit makes me insist
+## that a generator is a thing that gives integers (stubbornly)
 
 runif(1)
 
 ## What is the internal state of the generator?  I literally mean,
 ## what numbers summarize the machine that can generate random
-## bumbers. Run this to see. Don't hate me if it fills up a page.
+## numbers? Run this to see. Don't hate me if it fills up a page.
 
 .Random.seed
 
 ## That's 626 numbers, it summarizes the internal state of
 ## this particular generator in R.
+
+##
+## As you can see from .Random.seed, its internal state is quite a
+## complicated thing. Other random generator functions have shorter
+## states, so study .Random.seed, but also try to stay flexible.
 
 ## Lets save a copy of that
 
@@ -49,20 +67,18 @@ s1[1:20]
 
 ## 403
 
-## That's R's indicator for the type of the generator. Unless
-## something has changed dramatically, 403 refers to the default
-## random generator in R (and most good programs, actually), which is
-## MT19937.
+## That's R's indicator for the type of the generator.  403 is really
+## a "4" and an "03".  The 4 indicates the normal is simulated by
+## "inversion" while the "03" indicates that the generator is MT19937.
 
 ## Look at the second number, I see
 
 ## 1
 
-## That is a marker peculiar to the way MT19937 works.  It indicates
-## that this  particular vector has been used to draw one random number.
+## That is a COUNTER. It is a marker peculiar to the way MT19937
+## works.  It indicates that this particular vector has been used to
+## draw one random number.
 
-## As you can see from .Random.seed, its internal state is quite a
-## complicated thing.
 
 ## MT19937 is updated in an interesting way. The MT19937 does not
 ## update the entire state vector every time it needs to generate a
@@ -99,7 +115,7 @@ cbind(s1, s2, s3, s4)[1:20, ]
 ## to cause the 2nd variable to increment past 624--
 ## then the WHOLE VECTOR changes.
 
-trash <- runif(619)
+invisible(runif(619))
 
 ## That positions the state right at the end, at 624
 s5 <- .Random.seed
@@ -107,19 +123,21 @@ cbind(s4, s5)[1:10, ]
 
 
 ## Now, draw one more.
-trash <- runif(1)
+invisible(runif(1))
 s6 <- .Random.seed
 
 cbind(s1, s4, s5, s6)[1:10, ]
 ## See that? The whole thing snapped!
 
-## Do you want to force the generator back to an earlier state?
+## Draw a few more random uniform values, you
+## will see the counter start ticking up, inexorably,
+## toward 624, when the whole thing blows up again.
 
-## You can.
+
 
 ## Please note, just for clarity, it is the same thing
 ## to simply view the contents of the internal state with the command
-## > .Random.seed
+.Random.seed[1:10]
 ## as it is to use the get function:
 
 get(".Random.seed", envir = .GlobalEnv)[1 : 10]
@@ -128,8 +146,17 @@ get(".Random.seed", envir = .GlobalEnv)[1 : 10]
 ## leaves no mystery about which environment is storing
 ## the value .Random.seed.
 
+## Do you want to force the generator back to an earlier state?
+
+## You can. A command like
+
+## set.seed(12345)
+
+## will push it back to the original initial state.
+
 ## The counterpart of get() is assign(), which can
-## place a value into an environment.
+## place a value into an environment. Rather than
+## assigning .Random.seed with <-, this way is safer.
 
 assign("Random.seed", s1, envir=.GlobalEnv)
 
--- portableParallelSeeds/portableParallelSeeds/inst/examples/randomLesson-002.R
+++ portableParallelSeeds/portableParallelSeeds/inst/examples/randomLesson-002.R
@@ -1,18 +1,18 @@
-##Paul E. Johnson
-## pauljohn@ku.edu 2012-10-25
-set.seed(12345)
+## Paul E. Johnson
+## pauljohn@ku.edu 2012-10-29
 
-## Now, change the generator to a different type,
-## the CMRG (L'Ecuyer, 1999).
+## I carelessly lost the original writeup, but
+## have reconstructed with less commentary.
 
+## Change the generator type to the CMRG
+## and study the internal state by looking at
+## .Random.seed over and over
 
 RNGkind("L'Ecuyer-CMRG")
 set.seed(12345)
+t0 <- .Random.seed
 runif(1)
 t1 <- .Random.seed
-t1
-
-
 runif(1)
 t2 <- .Random.seed
 runif(1)
@@ -21,21 +21,99 @@ rnorm(1)
 t4 <- .Random.seed
 cbind(t1, t2, t3, t4)
 
+## Here is the really important working bit of the
+## "many substreams" approach of L'Ecuyer, et al.
+## It was offered in R's new parallel package for
+## the first time in version 2.14.
 
+require(parallel) ## for nextRNGStream
+substreams <- vector("list", 5)
+substreams[[1]] <- t0
+substreams[[2]] <- nextRNGStream(t0)
+substreams[[3]] <- nextRNGStream(substreams[[2]])
+substreams[[4]] <- nextRNGStream(substreams[[3]])
+substreams[[5]] <- nextRNGStream(substreams[[4]])
+substreams
+
+## Each of those can be used by assign() to
+## initialize the random generator at far-apart values
+## of the really long random vector (thus giving us
+## substreams that we know are separate).
+
+## Here's a fun fact. rnorm takes two values from the
+## generator, but runif and rpois only take one.
+
+
+## Why would this be interesting?  Try this:
+
+set.seed(12345)
+x1 <- runif(10)
+x2 <- rpois(10, lambda=7)
+x3 <- runif(10)
+
+set.seed(12345)
+y1 <- runif(10)
+y2 <- rnorm(10)
+y3 <- runif(10)
+
+identical(x1, y1) ## yeah
+identical(x2, y2) ## duh!
+identical(x3, y3) ## oh, no!
+
+## Shouldn't x3 and y3 be the same! This is baaadddd.
+
+## why did that happpen? The rnorm knocks twice at the generator's door.
+
+## That problem is easiest to see with MT19937
+
+RNGkind("Mersenne-Twister")
+set.seed(12345)
+invisible(runif(1)); s1 <- .Random.seed
+invisible(runif(1)); s2 <- .Random.seed
+invisible(runif(1)); s3 <- .Random.seed
+invisible(rnorm(1)); s4 <- .Random.seed
+cbind(s1, s2, s3, s4)[1:8, ]
+
+##Look at the counter, in the 2nd line. The
+## step from s3 to s4 gobbles up 2 values from
+## the generator.
+
+## Now re-start same generator sequence, but
+## draw 5 uniforms.
 
 assign(".Random.seed", t1, envir=.GlobalEnv)
 u1 <- .Random.seed
-runif(1)
+invisible(runif(1))
 u2 <- .Random.seed
-runif(1)
+invisible(runif(1))
 u3 <- .Random.seed
-runif(1)
+invisible(runif(1))
 u4 <- .Random.seed
-runif(1)
+invisible(runif(1))
 u5 <- .Random.seed
 cbind(u1, u2, u3, u4, u5)
 
+## Things to note. Given what I've said above, this:
+## set.seed(12345)
+##
+## is the same as this
+##
+## .Random.seed <- t1
+##
+## is the same as
+##
+## assign(.Random.seed, t1, envir=.GlobalEnv)
 
+## note the weirdness that rgamma may draw 2,3, or more
+## numbers from the generator. Easy to see with Mersenne-Twister.
 
-
+RNGkind("Mersenne-Twister")
+set.seed(12345)
+invisible(rgamma(1, shape = 1)); v1 <- .Random.seed[1:4]
+invisible(rgamma(1, shape = 1)); v2 <- .Random.seed[1:4]
+invisible(rgamma(1, shape = 1)); v3 <- .Random.seed[1:4]
+invisible(rgamma(1, shape = 1)); v4 <- .Random.seed[1:4]
+invisible(rgamma(1, shape = 1)); v5 <- .Random.seed[1:4]
+invisible(rgamma(1, shape = 1)); v6 <- .Random.seed[1:4]
+cbind(v1, v2, v3, v4, v5, v6)
 
