26d7b68ae3cb2f6e17d0d8496516823cce905958
Paul Johnson <pauljohn@ku.edu>
2012-11-01 17:52:18 +0000

    Overhauled buildPackage.sh to run as a single script. Note 1. now it has the roxygen command build inside it. 2. Uses PACKAGE name to be more abstract

--- portableParallelSeeds/buildPackage.sh
+++ portableParallelSeeds/buildPackage.sh
@@ -1,18 +1,39 @@
-rm -rf portableParallelSeeds.svnex
+#!/bin/sh
 
-svn export portableParallelSeeds portableParallelSeeds.svnex
-cd portableParallelSeeds.svnex/vignettes
+
+### 2012-11-1 adapts ideas from
+### http://stackoverflow.com/questions/4380849/roxygen-package-building-and-use-rd2-true
+
+PACKAGE="portableParallelSeeds"
+
+VERSION=$(awk -F": +" '/^Version/ { print $2 }' ${PACKAGE}/DESCRIPTION)
+
+rm -rf ${PACKAGE}.svnex
+
+svn export ${PACKAGE} ${PACKAGE}.svnex
+cd ${PACKAGE}.svnex/vignettes
+
 lyx -e pdf2 pps.lyx
 lyx -e sweave pps.lyx
-
-## cp -f pps.pdf ../inst/doc
+cp -f pps.pdf ../../${PACKAGE}/vignettes
 
 cd ../..
 
-R --vanilla -f runRoxygen2.R
+R --vanilla <<EOR
+library(roxygen2)
+roxygenize("${PACKAGE}.svnex")
 
-R CMD build portableParallelSeeds.svnex
+EOR
 
-read -p "enter name of tarball: " result
+R CMD build ${PACKAGE}.svnex
 
-R CMD check --as-cran $result
+read -p "Run check: OK? (y or n)" result
+
+if [ $result = "y" ];  then
+R CMD check --as-cran ${PACKAGE}_${VERSION}.tar.gz
+fi 
+
+read -p "Install: OK? (y or n)" result
+if [ $result = "y" ]; then
+R CMD INSTALL ${PACKAGE}_${VERSION}.tar.gz
+fi
